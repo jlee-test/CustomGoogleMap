0c4ed89b9005365a8810b575b85c6460f13bad7a
jlee-test <35935139+jlee-test@users.noreply.github.com>
2018-02-06 09:32:37 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiY2psZWVpaSIsImEiOiJjamQ2b3BiMnQwemRqMnlyMnpwbm11djNhIn0.Yh_KksP2zZy3WCuBbO5EGw';
+var mymap = L.mapbox.map('map', 'mapbox.streets', {
+	maxZoom: 20,
+	keyboardPanOffset: 100
+});
+
+mymap.locate({
+	setView: true,
+	watch: true,
+	maximumAge: 50000,
+	enableHighAccuracy: true
+}).on('locationfound', function(e) {
+	L.marker([e.latitude, e.longitude], {
+		title: 'You are here!'
+	}).addTo(mymap);
+}).on('locationerror', function(e) {
+	alert(":'(");
+});
+
+L.control.scale({
+	position: 'topleft',
+	updateWhenIdle: 'false'
+}).addTo(mymap);
+
+L.mapbox.featureLayer('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson').on('ready', function(e) {
+	var clusterGroup = new L.MarkerClusterGroup();
+	e.target.eachLayer(function(layer) {
+		clusterGroup.addLayer(layer);
+	});
+	mymap.addLayer(clusterGroup);
+});
+
+
+
+
+/*
+
+var gridLayer = L.mapbox.gridLayer('mapbox.light');
+mymap.addLayer(L.mapbox.tileLayer('mapbox.light'));
+mymap.addLayer(gridLayer);
+mymap.addControl(L.mapbox.gridControl(gridLayer));
+
+*//*
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.light',
+	accessToken: 'pk.eyJ1IjoiY2psZWVpaSIsImEiOiJjamQ2b3BiMnQwemRqMnlyMnpwbm11djNhIn0.Yh_KksP2zZy3WCuBbO5EGw'
+}).addTo(mymap);
+*/
